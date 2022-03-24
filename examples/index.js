f00dccdfe4b405288123fadf055c35ad850d30fe
Theryston Santos <funktodo2@gmail.com>
2022-03-24 15:21:43 -0300

    chore: add a example

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,24 @@
-// run yarn and yarn build before run this example
-const { GeneratePerson } = require('../dist');
+const { Certificates, CPF } = require('../dist');
 
-const person = new GeneratePerson('Will', 10);
+(async () => {
+  {
+    // CPF
+    const { cpf } = await CPF.generate({ isWithDots: true, stateCode: 'BA' }); // 34317074087
 
-console.log(person);
+    console.log(cpf);
+
+    const { isValid } = await CPF.validate({ cpf });
+
+    console.log(isValid);
+  }
+  {
+    // Certificates
+    const { certificate } = await Certificates.generate();
+
+    console.log(certificate);
+
+    const { isValid } = await Certificates.validate({ certificate });
+
+    console.log(isValid);
+  }
+})();
